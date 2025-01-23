8d3c3ceabd571bee251e9835bc04bd25e09a3dc3
Emmanuel Ashitey <eaashitey39@gmail.com>
2025-01-23 17:40:59 +0000

    add providers list here

--- lib/src/model/email_provider.dart
+++ lib/src/model/email_provider.dart
@@ -3,4 +3,44 @@ class EmailProvider {
   final List<String> domains;
 
   EmailProvider({required this.aliases, required this.domains});
+
+  // Define email providers and their aliases/domains
+  static final Map<String, EmailProvider> providers = {
+    "google": EmailProvider(
+      aliases: ["gmail", "googlemail"],
+      domains: [".com"],
+    ),
+    "yahoo": EmailProvider(
+      aliases: ["yahoo", "ymail", "rocketmail"],
+      domains: [
+        ".com",
+        ".co.uk",
+        ".fr",
+        ".de",
+        ".ca",
+        ".it",
+        ".au",
+        ".es",
+        ".br",
+        ".in",
+        ".j"
+      ],
+    ),
+    "microsoft": EmailProvider(
+      aliases: ["outlook", "hotmail", "live", "msn"],
+      domains: [".com", ".co", ".de", ".co.uk", ".nl"],
+    ),
+    "apple": EmailProvider(
+      aliases: ["icloud", "me", "mac"],
+      domains: [".com"],
+    ),
+    "yandex": EmailProvider(
+      aliases: ["yandex"],
+      domains: [".com", ".ru", ".ua", ".kz", ".by"],
+    ),
+    "fastmail": EmailProvider(
+      aliases: ["fastmail"],
+      domains: [".com", ".fm"],
+    ),
+  };
 }
