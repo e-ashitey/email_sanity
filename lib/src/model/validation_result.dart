c26142584d40369844f99c008d5b8a19a17d6bb7
Emmanuel Ashitey <eaashitey39@gmail.com>
2025-02-10 12:36:09 +0000

    use sanity localization instead

--- lib/src/model/validation_result.dart
+++ lib/src/model/validation_result.dart
@@ -4,13 +4,10 @@ class ValidationResult {
   final bool isValid;
 
   /// An optional error message if the email is invalid.
-  final EmailValidationError? error;
+  final String? error;
 
   /// Constructor to create a ValidationResult instance.
   ValidationResult({required this.isValid, this.error});
-
-  /// Get the error message if the email is invalid.
-  String? get errorMessage => error?.message;
 }
 
 /// An enum to represent the possible email validation errors.
@@ -50,39 +47,4 @@ enum EmailValidationError {
 
   /// The email alias does not match any supported provider aliases.
   disallowedTopLevelDomain,
-}
-
-/// Extension to add error messages
-extension EmailValidationErrorMessage on EmailValidationError {
-  /// Get the error message for the email validation error.
-  String get message {
-    switch (this) {
-      case EmailValidationError.requireEmail:
-        return "Email is required";
-      case EmailValidationError.invalidFormat:
-        return "The email format is invalid.";
-      case EmailValidationError.missingAtSymbol:
-        return "The email must contain the '@' symbol.";
-      case EmailValidationError.invalidDomain:
-        return "The email domain is invalid or does not include a '.' (e.g., gmail.com).";
-      default:
-        return "";
-      // case EmailValidationError.missingLocalPart:
-      //   return "The email is missing the local part before the '@' symbol.";
-      // case EmailValidationError.missingDomain:
-      //   return "The email is missing the domain part after the '@' symbol.";
-      // case EmailValidationError.domainNotSupported:
-      //   return "The email domain is not supported by this service.";
-      // case EmailValidationError.topLevelDomainNotAllowed:
-      //   return "The email uses a top-level domain that is not allowed.";
-      // case EmailValidationError.unknownProvider:
-      //   return "The email provider or alias is not recognized.";
-      // case EmailValidationError.invalidAlias:
-      //   return "The email alias does not match any supported provider aliases.";
-      // case EmailValidationError.internationalEmailNotSupported:
-      //   return "Internationalized email addresses are not supported.";
-      // case EmailValidationError.disallowedTopLevelDomain:
-      //   return "Emails with this top-level domain are not allowed.";
-    }
-  }
 }
